e61d21283b3fdad09e84a42f9b5a520ac38799b4
Korotkov Dmitry <2110821@mail.ru>
2021-07-16 23:09:49 +0300

    Код ПО. Версия 1.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialamount = 100;
+        int replenishmentamount = 100;
+        int limit = 1000;
+        int bonus;
+        if (replenishmentamount < limit) {
+            bonus = 0;
+        } else {
+            bonus = replenishmentamount / 100;
+        }
+        int finalamount = initialamount + replenishmentamount + bonus;
+        System.out.println(finalamount);
+    }
+}
