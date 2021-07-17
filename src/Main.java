bb3b5865cf800185ff7bfacb0cc9801c1c759d5b
Korotkov Dmitry <2110821@mail.ru>
2021-07-17 13:04:25 +0300

    Изменение названий переменных

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int initialamount = 100;
-        int replenishmentamount = 100;
+        int initialAmount = 100;
+        int replenishmentAmount = 100;
         int limit = 1000;
         int bonus;
-        if (replenishmentamount < limit) {
+        if (replenishmentAmount < limit) {
             bonus = 0;
         } else {
-            bonus = replenishmentamount / 100;
+            bonus = replenishmentAmount / 100;
         }
-        int finalamount = initialamount + replenishmentamount + bonus;
-        System.out.println(finalamount);
+        int finalAmount = initialAmount + replenishmentAmount + bonus;
+        System.out.println(finalAmount);
         System.out.println(bonus);
     }
 }
